ef5d46222ad8c4bc44f9ed0e63b4955e883cee3c
Jonathan-Paul-Wood <jpwood@udel.edu>
2020-11-28 10:17:20 -0500

    added button styling

--- src/common/Button/Button.js
+++ src/common/Button/Button.js
@@ -6,33 +6,94 @@ import PropTypes from 'prop-types';
 const StyleContainer = styled.div`
     margin: auto 0;
 
-    button {
-        border-radius: 5px;
-        text-transform: uppercase;
-        padding: 0.5rem;
-    }
-
-    button:active{
-        border: 1px solid #4dd0e1;
-        box-shadow: 0 0 0 1px #4dd0e1;
-    }
-
-    
-    button:hover{
-        box-shadow: 0 0 0 1px #4dd0e1;
+    --text-dark: #000000;
+    --text-dark-hover: #000000;
+    --text-white: #ffffff;
+    --text-light-hover: #ffffff;
+    --primary-color: #99bdff;
+    --primary-color-hover: #456cb5;
+    --secondary-color: #ffffff;
+    --secondary-color-hover: #e6e6e6;
+    --success-color: #76e02b;
+    --success-color-hover: #60a331;
+    --danger-color: #ff6e6e;
+    --danger-color-hover: #b54545;
+    --warning-color: #f2f241;
+    --warning-color-hover: #99993a;
+      
+      .btn {
+        padding: 0.5em 1em;
+        font-size: 15px;
+        border: none;
+        box-shadow: 0 0 2px var(--text-dark);
         cursor: pointer;
-    }
-
-    .btn-secondary {
-        background-color: #ffffff;
-    }
+        transition: background-color 0.2s ease;
+      }
+      
+      .btn:focus {
+        outline: 0;
+      }
+      
+      .btn:active {
+        transform: scale(0.97);
+      }
+      
+      .btn--primary {
+        color: var(--text-light);
+        background-color: var(--primary-color);
+      }
+      
+      .btn--primary:hover {
+        color: var(--text-light-hover);
+        background-color: var(--primary-color-hover);
+      }
+      
+      .btn--secondary {
+        color: var(--text-dark);
+        background-color: var(--secondary-color);
+      }
+      
+      .btn--secondary:hover {
+        color: var(--text-dark-hover);
+        background-color: var(--secondary-color-hover);
+      }
+      
+      .btn--success {
+        color: var(--text-light);
+        background-color: var(--success-color);
+      }
+      
+      .btn--success:hover {
+        color: var(--text-light-hover);
+        background-color: var(--success-color-hover);
+      }
+      
+      .btn--danger {
+        color: var(--text-light);
+        background-color: var(--danger-color);
+      }
+      
+      .btn--danger:hover {
+        color: var(--text-light-hover);
+        background-color: var(--danger-color-hover);
+      }
+      
+      .btn--warning {
+        color: var(--text-light);
+        background-color: var(--warning-color);
+      }
+      
+      .btn--warning:hover {
+        color: var(--text-light-hover);
+        background-color: var(--warning-color-hover);
+      }
 `;
 
 
 export default function Button(props) {
     return (
         <StyleContainer>
-            <button className={`btn btn-${props.type} btn-${props.size} ${props.block ? 'btn-block' : ''}`}>
+            <button className={`btn btn--${props.type} btn--${props.size} ${props.block ? 'btn-block' : ''}`}>
                 {props.label}
             </button>
         </StyleContainer>
