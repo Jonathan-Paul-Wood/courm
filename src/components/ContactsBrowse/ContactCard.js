ba255d4bbd6abac93e8c44213a63ec3543088aeb
Jonathan-Paul-Wood <jpwood@udel.edu>
2020-12-23 12:41:11 -0500

    improved contact card

--- src/components/ContactsBrowse/ContactCard.js
+++ src/components/ContactsBrowse/ContactCard.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import Button from '../../common/Button/Button';
 
 const CardWrapper = styled.div`
-    height: 50px;
+    height: 15vh;
     background: #f2f2f2;
     border-radius: 4px;
     box-shadow: 2px 3px 2px #cccccc;
     margin: 0.5rem;
     padding: 0.5rem;
-    display: flex;
-    justify-content: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    #head-wrapper {
+        display: flex;
+        justify-content: space-between;
+        margin: 0.5em;
+    }
+
+    .card-content {
+        margin: 0.5em;
+        display: flex;
+        justify-content: space-around;
+    }
 `;
 
 export default function ContactCard(props) {
-    const { name } = props;
+    const { contact } = props;
+    const history = useHistory();
 
     return (
         <CardWrapper>
-            {name}
+            <div id="head-wrapper">
+                <div>img</div>
+                <div>
+                    <b>{contact.firstName}</b>, {contact.lastName}
+                </div>
+                <Button
+                    label="View"
+                    onClick={() => history.push(`/contacts/${contact.id}`)}
+                />
+            </div>
+
+            <div id="communcate-wrapper" className="card-content">
+                <div>{contact.email}</div>
+                <div>{contact.phoneNumber}</div>
+            </div>
         </CardWrapper>
     );
 }
 
 ContactCard.propTypes = {
-    name: PropTypes.string.isRequired,
+    contact: PropTypes.object.isRequired,
 }
