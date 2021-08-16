85f8373f34ad8954dfcae86438b48740cb48c33e
Jonathan-Paul-Wood <jpwood@udel.edu>
2021-08-15 22:02:37 -0400

    update eslint to work with jest and unit tests

--- ui/src/common/Button/Button.test.js
+++ ui/src/common/Button/Button.test.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
 const props = {
     label: 'Test String',
     onClick: jest.fn()
-}
+};
 
 describe('Test Button status and actions', () => {
-    beforeEach(jest.resetAllMocks);
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
 
     it('renders active button', () => {
         render(<Button {...props} />);
         expect(screen.queryByText('Test String')).toBeInTheDocument();
+        fireEvent.click(screen.getByRole('button'));
+        expect(props.onClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not activate if disabled', () => {
+        render(<Button {...props} disabled={true} />);
+
+        fireEvent.click(screen.getByRole('button'));
+        expect(props.onClick).toHaveBeenCalledTimes(0);
+    });
+
+    it('does not activate if pending', () => {
+        render(<Button {...props} isPending={true} />);
+
+        fireEvent.click(screen.getByRole('button'));
+        expect(props.onClick).toHaveBeenCalledTimes(0);
     });
 });
-
-
-
