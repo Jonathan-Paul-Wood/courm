3c5df0b42761b7bb6acec14d0d7b9a1117b093e7
Jonathan-Paul-Wood <jpwood@udel.edu>
2021-08-18 13:57:11 -0400

    removed unused service files

--- ui/src/services/ExampleService.js
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from '../configs/axiosSingleton';
-
-export default class ExampleService {
-    static getExample () {
-        return axiosInstance.get('/app/api/example');
-    }
-}
