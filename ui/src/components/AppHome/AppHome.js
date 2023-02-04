457e3bda2304005dc1c425ce3cac593fae2fb282
Jonathan-Paul-Wood <jpwood@udel.edu>
2023-02-04 13:57:07 -0500

    added BETA tag and some copy

--- ui/src/components/AppHome/AppHome.js
+++ ui/src/components/AppHome/AppHome.js
@@ -12,6 +12,12 @@ const HomeWrapper = styled.div`
             background-color: #f2f2f2;
         }
     }
+
+    .link {
+        color: blue;
+        text-decoration-line: underline;
+        cursor: pointer;
+    }
 `;
 
 export default function AppHome (props) {
@@ -38,15 +44,19 @@ export default function AppHome (props) {
 
     return (
         <HomeWrapper>
-            <h2>Welcome</h2>
+            <h2>Welcome to CouRM <span style={{ color: 'red' }}>BETA</span></h2>
             <h4>Use this personal CRM / Address Book to keep track of your contacts!</h4>
-            <h2>Actions</h2>
+            <h2>Common Actions</h2>
             <ul className="action-list">
                 <li onClick={() => handleActionSelection('create', 'contacts')}>Create Contact</li>
                 <li onClick={() => navigate('/notes/new')}>Record Note</li>
                 <li onClick={() => handleActionSelection('view', 'contacts')}>View & Search Contacts</li>
                 <li onClick={() => navigate('/notes')}>View & Search Notes</li>
             </ul>
+            <h2>Background</h2>
+            <p>
+                We're looking to create an intuitive tool that lets you track your social and professional life without the all-seeing eye of social media and other panopticons. We're still very early in this journey and would love to hear your feedback! Checkout the <span className="link" onClick={() => navigate('/faq')}>FAG page</span> if you have questions, and have a wonderful day!
+            </p>
         </HomeWrapper>
     );
 }
