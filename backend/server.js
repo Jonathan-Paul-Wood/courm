ea3b4975c1cc6356f47276c0dae0a84ed27a64a6
Jonathan-Paul-Wood <jpwood@udel.edu>
2023-02-04 22:23:48 -0500

    added byRelation logic to fetch of notes; also fixed bug in all byRelation logic where second or third Map.set could overwrite previous writes in same loop

--- backend/server.js
+++ backend/server.js
@@ -186,25 +186,18 @@ app.get('/api/notes/all', (req, res) => {
 
 //accepts requests of the form: /api/notes?order=id?results=3&page=1?direction=[ASC|DESC]?search=string
 app.get("/api/notes", (req, res) => {
-    const { results, page, order, direction, searchTerm, filters } = req.query;
-    let sql = `SELECT * FROM notes`;
-
-    //apply search
-    if(searchTerm && filters) {
-        const searchFilters = filters.split(',');
-        sql = sql + ` WHERE ${searchFilters[0]} LIKE '%${searchTerm}%'`;
-        searchFilters.forEach((filter, index) => {
-            if(index) {
-                sql = sql + ` OR ${filter} LIKE '%${searchTerm}%'`
-            }
-        });
+    const { results, page, order, direction, searchTerm, filters, relatedContacts, relatedEvents } = req.query;
+    const relationships = {};
+    if (relatedContacts) {
+        relationships.contacts = relatedContacts.split(',');
+    }
+    if (relatedEvents) {
+        relationships.events = relatedEvents.split(',');
     }
 
-    const sql_metadata = sql;
-    //apply sort order and pagination
-    sql = sql+` ORDER BY ${order} ${direction} LIMIT ${results} OFFSET ((${page - 1})* ${results})`;
+    let sqlPre = `SELECT * FROM relations LEFT JOIN notes ON relations.noteId = notes.id`;
 
-    db.all(sql, (err, rows) => {
+    db.all(sqlPre, (err, rows) => {
         if (err) {
             console.log(err);
             res.status = ERROR_CODE;
@@ -213,22 +206,108 @@ app.get("/api/notes", (req, res) => {
             res.status = NOT_FOUND_CODE;
             res.json({message: 'NOT FOUND'});
         } else {
-            db.all(sql_metadata, (err, result) => {
+            const relatedRecordMap = new Map();
+
+            // create map where each record id has an object of their existing relationships
+            rows.forEach((row) => {
+                if (row.id) {
+                    if (!relatedRecordMap.get(row.id)) {
+                        relatedRecordMap.set(row.id, {
+                            contacts: [],
+                            events: [],
+                            notes: []
+                        });
+                    }
+                    const thisRecordEntry = relatedRecordMap.get(row.id);
+                    if (row.contactId && !thisRecordEntry.contacts.includes(row.contactId)) {
+                        relatedRecordMap.set(row.id, {
+                            ...thisRecordEntry,
+                            contacts: [...thisRecordEntry.contacts, row.contactId]
+                        });
+                    }
+                    if (row.eventId && !thisRecordEntry.events.includes(row.eventId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
+                        relatedRecordMap.set(row.id, {
+                            ...possiblyUpdatedRecord,
+                            events: [...possiblyUpdatedRecord.events, row.eventId]
+                        });
+                    }
+                    if (row.noteId && !thisRecordEntry.notes.includes(row.noteId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
+                        relatedRecordMap.set(row.id, {
+                            ...possiblyUpdatedRecord,
+                            notes: [...possiblyUpdatedRecord.notes, row.noteId]
+                        });
+                    }
+                }
+            });
+
+            // filter for just the record ids with the expected relations
+            const filteredIds = [];
+            const mapKeys = relatedRecordMap.keys();
+            let nextMapKey = mapKeys.next();
+            while (nextMapKey.done === false) {
+                const key = nextMapKey.value;
+                const recordRelations = relatedRecordMap.get(key);
+                const hasAllExpectedRelations = Object.keys(relationships).every((recordIdType) => {
+                    return relationships[recordIdType].every((id) => {
+                        return recordRelations[recordIdType].find(i => i === parseInt(id));
+                    });
+                });
+                if (hasAllExpectedRelations) {
+                    filteredIds.push(key);
+                }
+
+                nextMapKey = mapKeys.next();
+            }
+
+            let sql = `SELECT * FROM notes`;
+
+            //apply search
+            if(searchTerm && filters) {
+                const searchFilters = filters.split(',');
+                sql = sql + ` WHERE ${searchFilters[0]} LIKE '%${searchTerm}%'`;
+                searchFilters.forEach((filter, index) => {
+                    if(index) {
+                        sql = sql + ` OR ${filter} LIKE '%${searchTerm}%'`;
+                    }
+                });
+                sql = sql+` AND id IN (${filteredIds})`;
+            } else {
+                sql = sql+` WHERE id IN (${filteredIds})`;
+            }
+
+            const sql_metadata = sql;
+            //apply sort order and pagination
+            sql = sql+` ORDER BY ${order} ${direction} LIMIT ${results} OFFSET ((${page - 1})* ${results})`;
+
+            db.all(sql, (err, rows) => {
                 if (err) {
+                    console.log(err);
                     res.status = ERROR_CODE;
-                    return console.error(err.message);
-                } else if (!result) {
+                    res.json(err);
+                } else if (!rows) {
                     res.status = NOT_FOUND_CODE;
-                    return;
+                    res.json({message: 'NOT FOUND'});
                 } else {
-                    totalResults = result.length;
-                    res.json({
-                        results: rows,
-                        resultCount: rows.length,
-                        pageSize: parseInt(results),
-                        totalCount: totalResults,
-                        pageCount: Math.ceil(totalResults / parseInt(results)),
-                        currentPage: parseInt(page)
+                    db.all(sql_metadata, (err, result) => {
+                        if (err) {
+                            res.status = ERROR_CODE;
+                            return console.error(err.message);
+                        } else if (!result) {
+                            res.status = NOT_FOUND_CODE;
+                            return;
+                        } else {
+                            totalResults = result.length;
+                            res.json({
+                                results: rows,
+                                resultCount: rows.length,
+                                pageSize: parseInt(results),
+                                totalCount: totalResults,
+                                pageCount: Math.ceil(totalResults / parseInt(results)),
+                                currentPage: parseInt(page)
+                            });
+                        }
                     });
                 }
             });
@@ -427,15 +506,17 @@ app.get("/api/contacts", (req, res) => {
                         });
                     }
                     if (row.eventId && !thisRecordEntry.events.includes(row.eventId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
                         relatedRecordMap.set(row.id, {
-                            ...thisRecordEntry,
-                            events: [...thisRecordEntry.events, row.eventId]
+                            ...possiblyUpdatedRecord,
+                            events: [...possiblyUpdatedRecord.events, row.eventId]
                         });
                     }
                     if (row.noteId && !thisRecordEntry.notes.includes(row.noteId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
                         relatedRecordMap.set(row.id, {
-                            ...thisRecordEntry,
-                            notes: [...thisRecordEntry.notes, row.noteId]
+                            ...possiblyUpdatedRecord,
+                            notes: [...possiblyUpdatedRecord.notes, row.noteId]
                         });
                     }
                 }
@@ -667,15 +748,17 @@ app.get("/api/events", (req, res) => {
                         });
                     }
                     if (row.eventId && !thisRecordEntry.events.includes(row.eventId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
                         relatedRecordMap.set(row.id, {
-                            ...thisRecordEntry,
-                            events: [...thisRecordEntry.events, row.eventId]
+                            ...possiblyUpdatedRecord,
+                            events: [...possiblyUpdatedRecord.events, row.eventId]
                         });
                     }
                     if (row.noteId && !thisRecordEntry.notes.includes(row.noteId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
                         relatedRecordMap.set(row.id, {
-                            ...thisRecordEntry,
-                            notes: [...thisRecordEntry.notes, row.noteId]
+                            ...possiblyUpdatedRecord,
+                            notes: [...possiblyUpdatedRecord.notes, row.noteId]
                         });
                     }
                 }
@@ -988,15 +1071,17 @@ app.get("/api/records-by-relation/recordType/:recordType", async (req, res) => {
                         });
                     }
                     if (row.eventId && !thisRecordEntry.events.includes(row.eventId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
                         relatedRecordMap.set(row.id, {
-                            ...thisRecordEntry,
-                            events: [...thisRecordEntry.events, row.eventId]
+                            ...possiblyUpdatedRecord,
+                            events: [...possiblyUpdatedRecord.events, row.eventId]
                         });
                     }
                     if (row.noteId && !thisRecordEntry.notes.includes(row.noteId)) {
+                        const possiblyUpdatedRecord = relatedRecordMap.get(row.id);
                         relatedRecordMap.set(row.id, {
-                            ...thisRecordEntry,
-                            notes: [...thisRecordEntry.notes, row.noteId]
+                            ...possiblyUpdatedRecord,
+                            notes: [...possiblyUpdatedRecord.notes, row.noteId]
                         });
                     }
                 }
